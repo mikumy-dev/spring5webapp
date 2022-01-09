10011c354a20a37008d0bbfa345f336153c4c3ab
zosoyi <cjsz321qq@gmail.com>
2022-01-09 15:38:54 +0900

    18_add_publisher

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -17,12 +17,23 @@ public class Book {
     inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors = new HashSet<>();
 
+    @ManyToOne
+    private Publisher publisher;
+
     public Book(String title, String isbn) {
         this.title = title;
         this.isbn = isbn;
     }
 
     public Book() {
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
     }
 
     public Long getId() {
